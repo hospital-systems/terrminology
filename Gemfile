d0a0e2eff4a8397bfa0f783e617ad669527956d1
niquola <niquola@gmail.com>
2013-11-29 18:30:44 +0400

    cleanup specs

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in FHIR_value_sets.gemspec
 gemspec
 gem 'pg'
+gem 'rspec'
 gem 'sequel'
 gem 'nokogiri'
 gem 'virtus'
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -20,35 +20,36 @@ describe Terrminology do
     sys.clear_value_sets!
 
     vs_def = JSON.parse(rfile('valueset-status.json'))
+    idn = 'http://hl7.org/fhir/vs/valueset-status'
 
-    sys.value_sets.find {|vs|
-      vs.identifier == 'http://hl7.org/fhir/vs/valueset-status'
-    }.should be_nil
+    sys.value_sets.find{|vs| vs.identifier == idn }.should be_nil
 
     vs = sys.create_value_set(vs_def)
 
-    sys.value_sets.find {|v|
-      v.identifier == 'http://hl7.org/fhir/vs/valueset-status'
-    }.should_not be_nil
+    sys.value_sets.find{|v| v.identifier == idn }.should_not be_nil
 
     sys.find_value_set(vs.identifier).identifier.should == vs.identifier
 
     sys.concepts(vs.identifier).find{|c| c.code == 'active'}.should_not be_nil
     sys.concepts(vs.identity).find{|c| c.code == 'active'}.should_not be_nil
 
-    sys.concepts(vs.identifier, code: 'active').first.should == sys.concepts(vs.identifier).find{|c| c.code == 'active'}
-    sys.concepts(vs.identity,   code: 'active').first.should == sys.concepts(vs.identity).find{|c| c.code == 'active'}
+    sys.concepts(vs.identifier, code: 'active')
+    .first.should == sys.concepts(vs.identifier).find{|c| c.code == 'active'}
 
-    sys.concepts(vs.identifier,    code: 'reactivated', display: 'Reactivated', definition: 'Reactivated').should be_empty
-    concept = sys.add_concept(vs.identifier, code: 'reactivated', display: 'Reactivated', definition: 'Reactivated')
-    sys.concepts(vs.identifier,    code: 'reactivated', display: 'Reactivated', definition: 'Reactivated').should_not be_empty
+    sys.concepts(vs.identity, code: 'active')
+    .first.should == sys.concepts(vs.identity).find{|c| c.code == 'active'}
+
+    attrs = { code: 'reactivated', display: 'Reactivated', definition: 'Reactivated'}
+    sys.concepts(vs.identifier, attrs).should be_empty
+    concept = sys.add_concept(vs.identifier, attrs)
+    sys.concepts(vs.identifier, attrs).should_not be_empty
 
     concepts_amount = sys.concepts(vs.identity).size
 
     concepts_amount.should_not == 0
     sys.remove_concept(concept.identity)
-    sys.concepts(vs.identifier,     code: 'reactivated', display: 'Reactivated', definition: 'Reactivated').should be_empty
-    sys.concepts(vs.identity).size.should == concepts_amount - 1
 
+    sys.concepts(vs.identifier, attrs).should be_empty
+    sys.concepts(vs.identity).size.should == concepts_amount - 1
   end
 end
